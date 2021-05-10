dca27bd72f2e1ca5abb27f89ca796dbc5ae21245
Nikita Abramov <nikkita.abramov@gmail.com>
2021-05-10 22:53:37 +0200

    add eslint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
-import userName from "/users/nikitaabramov/frontend-project-lvl1/src/cli.js";
-console.log("Welcome to the Brain Games!");
+import userName from '../src/cli.js';
+
+console.log('Welcome to the Brain Games!');
 userName();
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
+
 const userName = () => {
-  const name = readlineSync.question("May i have your name? ");
-  return console.log("Hi, " + name + "!");
+  const name = readlineSync.question('May i have your name? ');
+  return console.log(`Hi, ${name}!`);
 };
 export default userName;
