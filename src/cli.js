643b33394b2f4e4d3e7abd7f16c69d442d2c5c2f
Nikita Abramov <nikkita.abramov@gmail.com>
2021-07-11 00:16:36 +0200

    fixed hexlet tests

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,6 @@ import readlineSync from 'readline-sync';
 
 const welcome = () => {
   const name = readlineSync.question('May i have your name? ');
-  return console.log(`Hi, ${name}!`);
+  console.log(`Hi, ${name}!`);
 };
 export default welcome;
