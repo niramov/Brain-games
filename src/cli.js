97d39f0619903228ed89d9be2b2e9ca036aaa032
Nikita Abramov <nikkita.abramov@gmail.com>
2021-07-10 23:59:32 +0200

    fixed hexlet tests

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
 import readlineSync from 'readline-sync';
 
 const welcome = () => {
-  console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May i have your name? ');
   return console.log(`Hi, ${name}!`);
 };
