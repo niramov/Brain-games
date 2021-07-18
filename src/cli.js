95582819e5f983fc354656e2d504a69e6ba6ee56
Nikita Abramov <nikkita.abramov@gmail.com>
2021-07-18 15:52:28 +0200

    fixed cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const welcome = () => {
-  const name = readlineSync.question('May i have your name? ');
+  const name = readlineSync.question('May I have your name?');
   console.log(`Hi, ${name}!`);
 };
 export default welcome;
