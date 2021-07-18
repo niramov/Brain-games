5f42747dd895e77f0c68deda8afd89b5287995ce
Nikita Abramov <nikkita.abramov@gmail.com>
2021-07-18 16:11:59 +0200

    add for of to brain-gcd

--- src/game-prime/gameLogic.js
+++ src/game-prime/gameLogic.js
@@ -3,21 +3,24 @@ import isPrimeNum from './isPrimeNum.js';
 import getRandomInt from './getRandomInt.js';
 
 const startGame = (name) => {
-  const num = getRandomInt(1, 100);
+  // eslint-disable-next-line space-in-parens
+  for (let i = 1; i <= 3; ) {
+    const num = getRandomInt(1, 100);
 
-  const correctAnswer = isPrimeNum(num);
+    const correctAnswer = isPrimeNum(num);
 
-  console.log(`Question: ${num}`);
+    console.log(`Question: ${num}`);
 
-  const answer = readlineSync.question('Your answer: ');
+    const answer = readlineSync.question('Your answer: ');
 
-  if (answer === correctAnswer) {
-    console.log('Correct!');
-  } else {
-    return console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+      i += 1;
+    } else {
+      return console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.
   Let's try again, ${name}!`);
+    }
   }
-
   return console.log(`Congratulations, ${name}!`);
 };
 
